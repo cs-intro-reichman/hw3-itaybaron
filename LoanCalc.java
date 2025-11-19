ea432d181f072cab37be9155368f6db46fb496ce
itay BARON <itaybaron@Mac.lan>
2025-11-19 23:01:02 +0200

    Comleted assignment

--- LoanCalc.java
+++ LoanCalc.java
@@ -5,88 +5,72 @@ public class LoanCalc {
 	static int iterationCounter;    // Number of iterations 
 	
 	// Gets the loan data and computes the periodical payment.
-    // Expects to get three command-line arguments: loan amount (double),
-    // interest rate (double, as a percentage), and number of payments (int).  
 	public static void main(String[] args) {		
-		// Gets the loan data
 		double loan = Double.parseDouble(args[0]);
 		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
+
 		System.out.println("Loan = " + loan + ", interest rate = " + rate + "%, periods = " + n);
 
-		// Computes the periodical payment using brute force search
+		// Brute force
 		System.out.print("\nPeriodical payment, using brute force: ");
 		System.out.println((int) bruteForceSolver(loan, rate, n, epsilon));
 		System.out.println("number of iterations: " + iterationCounter);
 
-		// Computes the periodical payment using bisection search
+		// Bisection
 		System.out.print("\nPeriodical payment, using bi-section search: ");
 		System.out.println((int) bisectionSolver(loan, rate, n, epsilon));
 		System.out.println("number of iterations: " + iterationCounter);
 	}
 
-	// Computes the ending balance of a loan, given the loan amount, the periodical
-	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
+	// Computes ending balance
 	private static double endBalance(double loan, double rate, int n, double payment) {	
 		double balance = loan;
-		double r = (rate / 100.0) / n;
-        
-        for (int i = 0; i < n; i++) {
-            balance = balance * (1 + r) - payment;
-        }
-        return balance;
+		double r = rate / 100.0;   // ריבית תקופתית
+
+		for (int i = 0; i < n; i++) {
+			balance = balance * (1 + r) - payment;
+		}
+		return balance;
 	}
-	
-	// Uses sequential search to compute an approximation of the periodical payment
-	// that will bring the ending balance of a loan close to 0.
-	// Given: the sum of the loan, the periodical interest rate (as a percentage),
-	// the number of periods (n), and epsilon, the approximation's accuracy
-	// Side effect: modifies the class variable iterationCounter.
-    public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
+
+	// Brute force search
+	public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
 		iterationCounter = 0;
 
-        double g = loan / n;
+		double g = loan / n;   // f(g) > 0
 
-        while (endBalance(loan, rate, n, g) > 0) {
-            g += epsilon;
-            iterationCounter++;
-        }
+		while (endBalance(loan, rate, n, g) > 0) {
+			g += epsilon;
+			iterationCounter++;
+		}
 
-        return g;
-    }
-    
-    // Uses bisection search to compute an approximation of the periodical payment 
-	// that will bring the ending balance of a loan close to 0.
-	// Given: the sum of the loan, the periodical interest rate (as a percentage),
-	// the number of periods (n), and epsilon, the approximation's accuracy
-	// Side effect: modifies the class variable iterationCounter.
-   public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-    iterationCounter = 0;
+		return g;
+	}
 
-    double r = (rate / 100.0) / n;
+	// Bisection search
+	public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
+		iterationCounter = 0;
 
-    double lo = loan / n;    // f(lo) > 0
-    double hi = loan;        // f(hi) < 0 ALWAYS
+		double lo = loan / n;   // f(lo) > 0
+		double hi = loan;       // f(hi) < 0 ALWAYS  
 
-    double fLo = endBalance(loan, rate, n, lo);
-    double fHi = endBalance(loan, rate, n, hi);
+		double fLo = endBalance(loan, rate, n, lo);
 
-    while (hi - lo > epsilon) {
-        iterationCounter++;
+		while (hi - lo > epsilon) {
+			iterationCounter++;
 
-        double mid = (lo + hi) / 2;
-        double fMid = endBalance(loan, rate, n, mid);
+			double mid = (lo + hi) / 2;
+			double fMid = endBalance(loan, rate, n, mid);
 
-        if (fMid * fLo > 0) {
-            lo = mid;
-            fLo = fMid;
-        } else {
-            hi = mid;
-            fHi = fMid;
-        }
-    }
+			if (fMid * fLo > 0) {
+				lo = mid;
+				fLo = fMid;
+			} else {
+				hi = mid;
+			}
+		}
 
-    return (lo + hi) / 2;
- 
- }
+		return (lo + hi) / 2;
+	}
 }
